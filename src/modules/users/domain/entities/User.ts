fae5bca522f3a438e28db09727974e1286578dee
Franklin Sales <franklin.salesx@gmail.com>
2025-05-14 23:05:01 -0300

    feat: create first version of User pure entity

---
+++ src/modules/users/domain/entities/User.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public email: string,
+    public role: 'client' | 'provider',
+  ){
+    // Add domain validations here
+  }
+}
